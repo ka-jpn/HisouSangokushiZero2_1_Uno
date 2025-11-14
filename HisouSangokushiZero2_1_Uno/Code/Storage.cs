e14c511fed9d48846404e7b534247e9aaaf40f3c
かりんとう <tomo_hirakawa_aomori@outlook.jp>
2025-11-14 23:19:36 +0900

    sdkバージョンがセーブ・ロード機能に支障があったためフォールバック

--- HisouSangokushiZero2_1_Uno/Code/Storage.cs
+++ HisouSangokushiZero2_1_Uno/Code/Storage.cs
@@ -2,7 +2,6 @@
 using MessagePack;
 using System;
 using System.IO;
-using System.Threading.Tasks;
 using Windows.Storage;
 using static HisouSangokushiZero2_1_Uno.Code.DefType;
 namespace HisouSangokushiZero2_1_Uno.Code;
@@ -13,9 +12,13 @@ internal static class Storage {
     return await ApplicationData.Current.LocalFolder.CreateFolderAsync(BaseData.name.Value,CreationCollisionOption.OpenIfExists);
   }
   internal static async Task WriteStorageData(GameState game,int fileNo) {
-    (await GetStorageFolder()).MyApplyA(myFolder => File.WriteAllBytes(Path.Combine(myFolder.Path,CreateSaveFileName(fileNo)),MessagePackSerializer.Serialize(game)));
+    //string writePath = CreateSaveFileName(fileNo);
+    string writePath = (await GetStorageFolder()).MyApplyF(myFolder => Path.Combine(myFolder.Path,CreateSaveFileName(fileNo)));
+    File.WriteAllBytes(writePath,MessagePackSerializer.Serialize(game));
   }
   internal static async Task<(ReadSaveFile,GameState?)> ReadStorageData(int fileNo) {
-    return (await GetStorageFolder()).MyApplyF(myFolder => Path.Combine(myFolder.Path,CreateSaveFileName(fileNo)).MyApplyF(readPath => !File.Exists(readPath) ? (ReadSaveFile.NotFind, null) : File.ReadAllBytes(readPath).MyApplyF(v => (ReadSaveFile.Read,MessagePackSerializer.Deserialize<GameState?>(v)))));
+    //string readPath = CreateSaveFileName(fileNo);
+    string readPath = (await GetStorageFolder()).MyApplyF(myFolder => Path.Combine(myFolder.Path,CreateSaveFileName(fileNo)));
+    return !File.Exists(readPath) ? (ReadSaveFile.NotFind, null) : (ReadSaveFile.Read,MessagePackSerializer.Deserialize<GameState?>(File.ReadAllBytes(readPath)));
   }
 }
--- HisouSangokushiZero2_1_Uno/Platforms/WebAssembly/Program.cs
+++ HisouSangokushiZero2_1_Uno/Platforms/WebAssembly/Program.cs
@@ -1,7 +1,7 @@
 using HisouSangokushiZero2_1_Uno;
 using Uno.UI.Hosting;
-await Uno.UI.Xaml.Media.FontFamilyHelper.PreloadAsync("ms-appx:///Assets/Fonts/SourceHanSansJP-Medium.woff2",new(500),Windows.UI.Text.FontStretch.Normal,Windows.UI.Text.FontStyle.Normal);
-await Uno.UI.Xaml.Media.FontFamilyHelper.PreloadAsync("ms-appx:///Assets/Fonts/SourceHanSansJP-Bold.woff2",new(700),Windows.UI.Text.FontStretch.Normal,Windows.UI.Text.FontStyle.Normal);
+_ = Uno.UI.Xaml.Media.FontFamilyHelper.PreloadAsync("ms-appx:///Assets/Fonts/SourceHanSansJP-Medium.woff2",new(500),Windows.UI.Text.FontStretch.Normal,Windows.UI.Text.FontStyle.Normal);
+_ = Uno.UI.Xaml.Media.FontFamilyHelper.PreloadAsync("ms-appx:///Assets/Fonts/SourceHanSansJP-Bold.woff2",new(700),Windows.UI.Text.FontStretch.Normal,Windows.UI.Text.FontStyle.Normal);
 var host = UnoPlatformHostBuilder.Create()
     .App(() => new App())
     .UseWebAssembly()
